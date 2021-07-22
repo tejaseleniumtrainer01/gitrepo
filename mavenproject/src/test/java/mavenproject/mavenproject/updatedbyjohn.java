b33502ea9d782487d6b17937cc00783a1dd2d9f7
tejas <tejas@DESKTOP-BD2AFT7.localdomain>
2021-07-21 22:00:26 -0400

    updated code

--- mavenproject/src/test/java/mavenproject/mavenproject/updatedbyjohn.java
+++ mavenproject/src/test/java/mavenproject/mavenproject/updatedbyjohn.java
@@ -1,1 +1,10 @@
-Hi
+package mavenproject.mavenproject;
+
+public class updatedbyjohn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How are you");
+	}
+
+}
