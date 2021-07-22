108cda5d68b7f46c436d61397d9e342ab830c8be
tejas <tejas@DESKTOP-BD2AFT7.localdomain>
2021-07-21 21:51:51 -0400

    New class file is created and POM file is updated with new dependencies

---
+++ mavenproject/src/test/java/mavenproject/mavenproject/printstatement.java
@@ -0,0 +1,10 @@
+package mavenproject.mavenproject;
+
+public class printstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
