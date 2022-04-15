f0a73c510b1728bdc58f994b7d25425492bcc81f
tejas <tejas@DESKTOP-BD2AFT7.localdomain>
2022-04-15 06:51:12 -0400

    updated code

--- mavenproject/src/test/java/mavenproject/mavenproject/printstatement.java
+++ mavenproject/src/test/java/mavenproject/mavenproject/printstatement.java
@@ -5,6 +5,7 @@ public class printstatement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		System.out.println("How are you?");
 		System.out.println("Bye");
 		
 	}
